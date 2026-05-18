ce48de2850a63d6e7d83e3cdaa61873b33066193
Muhammad Bilal Abdullah <engrmbilalabdullah@gmail.com>
2026-05-18 14:06:33 +0500

    Update rule_detector.py

--- detectors/rule_detector.py
+++ detectors/rule_detector.py
@@ -1,6 +1,6 @@
 class RuleDetector:
     def __init__(self):
-        # Yeh hamari khatarnak words ki list hai aur unke scores (0.0 se 1.0 tak)
+    
         self.attack_library = {
             "ignore all previous": 0.8,
             "jailbreak": 0.9,
@@ -12,19 +12,17 @@ class RuleDetector:
         }
 
     def analyze(self, text):
-        # Text ko chotay huroof (lowercase) mein convert karna
+      
         text_lower = text.lower()
         total_risk_score = 0.0
         
-        # Ek ek karke word check karna
+ 
         for pattern, weight in self.attack_library.items():
             if pattern in text_lower:
                 total_risk_score += weight
                 
-        # Score ko maximum 1.0 tak rakhna (taake ML aur Rule dono ka score same format mein rahay)
         return min(total_risk_score, 1.0)
 
-# Yeh hissa sirf test karne ke liye hai
 if __name__ == "__main__":
     detector = RuleDetector()
     print("Test 1 (Safe):", detector.analyze("Hello AI, how are you?"))
